3a1e2a6c28fb45d741a3ac202d593a9d10de2b9b
alibakhtiari <ali.bakhtiarii@gmail.com>
2025-12-14 19:44:23 +0330

    chore: configure static export settings

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import tseslint from "typescript-eslint";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ["dist", "api"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
