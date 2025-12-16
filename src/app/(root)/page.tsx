5e80d299827533a434ea0a7b91b2a463e21ba1a2
alibakhtiari <ali.bakhtiarii@gmail.com>
2025-12-16 18:05:53 +0330

    Update page.tsx

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,14 @@
-import { redirect } from 'next/navigation';
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function RootPage() {
-    // This generates a static HTML file with a meta-refresh tag.
-    // It effectively contains 0kb of client-side JavaScript.
-    redirect('/fa');
+    const router = useRouter();
+
+    useEffect(() => {
+        router.replace('/fa');
+    }, [router]);
+
+    return null;
 }
