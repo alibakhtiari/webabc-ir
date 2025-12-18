da4c0b23050c4f7e4dcf1aeed3e26115195b42ca
alibakhtiari <ali.bakhtiarii@gmail.com>
2025-12-18 14:23:33 +0330

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,17 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: process.cwd(),
   },
+  experimental: {
+    viewTransition: true,
+    inlineCss: true,
+    optimizePackageImports: [
+      'lucide-react',
+      'zod',
+      'sonner',
+      'date-fns',
+      'lodash'
+    ],
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
